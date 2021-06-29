425152c9077e120590568c50051ba41bb314be04
Zil Norvilis <zilasino27@gmail.com>
2021-06-29 15:56:18 +0200

    Add interface functionality

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const socket = require("socket.io");
+const express = require("express");
+const app = express();
+const path = require("path");
 
+app.use(express.static(path.join(__dirname, "")));
+
+server = app.listen(3000);
+
+const io = require("socket.io");
+
+const PORT = 3000 || process.env.PORT;
+console.log(`Server running on port: ${PORT}`);
